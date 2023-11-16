eb35d6c3b06c99b147bb184ddcd400739e82f430
gauravc574 <gauravchawre580@gmail.com>
2023-11-16 23:58:16 +0530

    Register view done

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from .forms import CreateUserForm, LoginForm
 
 
+# Home
 def home(request):
     # return HttpResponse("Hi , working")
     return render(request, 'webapp/index.html')
+
+# Register
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid:
+            form.save()
+    
+    context = {'form':form}
+    return render(request,'webapp/register.html',context)
